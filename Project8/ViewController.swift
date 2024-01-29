7ce6c0d97442e1c28fe7184c0bba755a9ea9b99a
o-pylypiv <bassenia@gmail.com>
2024-01-29 16:07:22 +0200

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Olha Pylypiv on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
